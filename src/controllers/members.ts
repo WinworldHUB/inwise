d980e8fccd7ab28eb510228dcb2b275c12720e11
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-04-24 19:51:10 +0530

    Add crud operations

--- src/controllers/members.ts
+++ src/controllers/members.ts
@@ -4,20 +4,163 @@ import { Member, CompanyRole, CreateMemberInput } from "../awsApis";
 import awsExport from "../amplifyconfiguration.json";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/api";
-import { createMember } from "../graphql/mutations";
-import { SignInInput, SignUpInput, confirmSignIn, confirmSignUp, signIn, signUp } from "aws-amplify/auth";
+import { createMember, updateMember } from "../graphql/mutations";
+import {
+  SignInInput,
+  SignUpInput,
+  confirmSignIn,
+  confirmSignUp,
+  signIn,
+  signUp,
+} from "aws-amplify/auth";
 import { isValidMember } from "../utils/member-util";
 import { Credentials } from "../types";
+import { getMember, listMembers } from "../graphql/queries";
 Amplify.configure(awsExport);
 
 const client = generateClient();
-export const getAllMembersHandler: RequestHandler = (req, res, next) => {};
 
-export const getMemberHandler: RequestHandler = (req, res, next) => {};
+export const getAllMembersHandler: RequestHandler = async (req, res, next) => {
+  try {
+    const members = await client.graphql({
+      query: listMembers,
+      variables: {
+        filter: {
+          isActive: {
+            eq: true,
+          },
+        },
+      },
+    });
 
-export const createMemberHandler: RequestHandler = (req, res, next) => {};
+    if (!members.data.listMembers.items) {
+      res.status(404).json({ error: "No members found" });
+      return;
+    }
 
-export const updateMemberHandler: RequestHandler = (req, res, next) => {};
+    res.json(members.data.listMembers.items);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to retrieve members" });
+  }
+};
+
+export const getMemberHandler: RequestHandler = async (req, res, next) => {
+  try {
+    const members = await client.graphql({
+      query: getMember,
+      variables: {
+        id: req.params.id,
+      },
+    });
+
+    res.json(members.data.getMember);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to retrieve members" });
+  }
+};
+
+export const getMemberByEmail: RequestHandler = async (req, res, next) => {
+  try {
+    const email = req.params.email.toLowerCase();
+    if (!email) {
+      res.status(400).json({ error: "Email parameter missing" });
+      return;
+    }
+    const members = await client.graphql({
+      query: listMembers,
+      variables: {
+        filter: {
+          email: { eq: email },
+        },
+      },
+    });
+    if (!members.data.listMembers.items) {
+      res.status(404).json({ error: "No member found" });
+      return;
+    }
+    res.json(members.data.listMembers.items[0]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to retrieve members" });
+  }
+};
+
+export const createMemberHandler: RequestHandler = async (req, res, next) => {
+  try {
+    const member = req.body as Member;
+    member.email = member.email.toLowerCase();
+    if (member) {
+      const newMember = await client.graphql({
+        query: createMember,
+        variables: {
+          input: member,
+        },
+      });
+
+      const createdMember = newMember.data.createMember;
+      if (!createdMember) {
+        return res.status(500).send({
+          status: "failed",
+          message: "Error saving member",
+        });
+      }
+      res.json(createdMember);
+    }
+  } catch (error) {
+    return res.status(500).send({
+      status: "failed",
+      message: "Error saving member",
+      internalError: error,
+    });
+  }
+};
+
+export const updateMemberHandler: RequestHandler = async (req, res, next) => {
+  try {
+    const email = req.params.email;
+    const requestBody = req.body;
+    if (email) {
+      const foundMember = await client.graphql({
+        query: listMembers,
+        variables: {
+          filter: {
+            email: { eq: email },
+          },
+        },
+      });
+
+      if (foundMember && foundMember.data.listMembers.items.length > 0) {
+        const memberToUpdate = foundMember.data.listMembers.items[0];
+
+        const input = {
+          ...requestBody,
+        };
+
+        const updatedMember = await client.graphql({
+          query: updateMember,
+          variables: {
+            input: input,
+            condition: { email: { eq: memberToUpdate.email } },
+          },
+        });
+
+        res.json(updatedMember.data.updateMember);
+      } else {
+        res.status(404).json({ error: "Member not found" });
+      }
+    } else {
+      res.status(400).json({ error: "Email parameter missing" });
+    }
+  } catch (error) {
+    return res.status(500).send({
+      status: "failed",
+      message: "Error updating member",
+      internalError: error,
+    });
+  }
+};
 
 export const deleteMemberHandler: RequestHandler = (req, res, next) => {};
 
@@ -92,12 +235,22 @@ export const SignInHandler: RequestHandler = async (req, res, next) => {
     // Sign in the member
     const memberSignInDetails = await signIn(signInDetails);
     if (memberSignInDetails) {
-      return res.status(200).json({ message: "Sign in Successful", memberSignInDetails });
+      return res
+        .status(200)
+        .json({ message: "Sign in Successful", memberSignInDetails });
     } else {
-      return res.status(500).json({ message: "Failed to sign in member", });
+      return res.status(500).json({ message: "Failed to sign in member" });
     }
-
   } catch (error) {
     res.status(500).json({ message: "Cannot login, please try again later" });
+  }
+};
+
+export const changePasswordHandler: RequestHandler = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Cannot Change Password, please try again later" });
   }
 };
